90ced401df07400054d00724f95616a1dadca7df
Masanori KUSUNOKI <masanork@gmail.com>
2025-12-30 08:34:31 +0900

    test: regenerate test root keys and update associated test data

--- sites/srn/static/mkform.js
+++ sites/srn/static/mkform.js
@@ -1,4 +1,4 @@
-window.__WEBA_BUILD_TIME__='2025-12-29T21:57:04Z';
+window.__WEBA_BUILD_TIME__='2025-12-29T23:31:45Z';
 var __create = Object.create;
 var __getProtoOf = Object.getPrototypeOf;
 var __defProp = Object.defineProperty;
