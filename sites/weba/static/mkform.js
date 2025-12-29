1ffe212cc19e33d4c168ea28231ea2edf8229004
Masanori KUSUNOKI <masanork@gmail.com>
2025-12-29 12:30:01 +0900

    feat: Update Web/A security presentation with new content and structure

--- sites/weba/static/mkform.js
+++ sites/weba/static/mkform.js
@@ -1,4 +1,4 @@
-window.__WEBA_BUILD_TIME__='2025-12-29T03:25:28Z';
+window.__WEBA_BUILD_TIME__='2025-12-29T03:26:15Z';
 var __create = Object.create;
 var __getProtoOf = Object.getPrototypeOf;
 var __defProp = Object.defineProperty;
