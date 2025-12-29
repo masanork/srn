e666e804ab5334cbd3bf85a7bb61fa0a6ec85630
Masanori KUSUNOKI <masanork@gmail.com>
2025-12-29 13:27:15 +0900

    feat: Add CLI commands and design documentation for Web/A Folio; update build times in mkform.js

--- sites/srn/static/mkform.js
+++ sites/srn/static/mkform.js
@@ -1,4 +1,4 @@
-window.__WEBA_BUILD_TIME__='2025-12-29T04:05:40Z';
+window.__WEBA_BUILD_TIME__='2025-12-29T04:26:15Z';
 var __create = Object.create;
 var __getProtoOf = Object.getPrototypeOf;
 var __defProp = Object.defineProperty;
