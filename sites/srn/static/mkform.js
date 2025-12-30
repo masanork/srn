81e71c7cf9f5fbbf953a00346ff7ff36a0caf268
Masanori KUSUNOKI <masanork@gmail.com>
2025-12-30 09:57:28 +0900

    docs: Add Guest DID with Passkey Authentication to L2E spec
    - Documented two use cases: anonymous (form DID) vs reply-enabled (guest DID)
    - Added Passkey-based authentication flow with sequence diagram
    - Defined guest DID document structure with expiration
    - Specified security considerations and requirements
    - Non-Passkey devices explicitly not supported

--- sites/srn/static/mkform.js
+++ sites/srn/static/mkform.js
@@ -1,4 +1,4 @@
-window.__WEBA_BUILD_TIME__='2025-12-30T00:23:37Z';
+window.__WEBA_BUILD_TIME__='2025-12-30T00:49:59Z';
 var __create = Object.create;
 var __getProtoOf = Object.getPrototypeOf;
 var __defProp = Object.defineProperty;
