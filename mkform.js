6294cf17c5f265f401c238dccc1b1384e7dfcb96
Masanori KUSUNOKI <masanork@gmail.com>
2025-12-30 23:23:21 +0900

    Updates

--- mkform.js
+++ mkform.js
@@ -1,4 +1,4 @@
-window.__WEBA_BUILD_TIME__='2025-12-30T14:12:03Z';
+window.__WEBA_BUILD_TIME__='2025-12-30T14:22:56Z';
 var __create = Object.create;
 var __getProtoOf = Object.getPrototypeOf;
 var __defProp = Object.defineProperty;
